a4d5bf424378268d4a2e924866c8a6624bd68d95
Rahul Dutta <125856707+rahuldutta0487@users.noreply.github.com>
2026-05-02 17:43:47 +0530

    Extend EcommerceApplication to support servlet initialization
    f

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,10 +1,19 @@
 package com.ecommerce;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication {
-public static void main(String[] args) {
-SpringApplication.run(EcommerceApplication.class, args);
-}
+public class EcommerceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcommerceApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
